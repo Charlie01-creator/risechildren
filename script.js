989235ef91ea9a4e38d34c15946114698f9639b7
Charlie01-creator <semakulacharles16@gmail.com>
2025-01-13 16:16:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+﻿function showCryptoForm() {
+    document.getElementById("crypto-form").classList.remove("hidden");
+    document.getElementById("bank-form").classList.add("hidden");
+    document.getElementById("credit-card-form").classList.add("hidden");
+    document.getElementById("paypal-form").classList.add("hidden");
+}
+
+function showBankForm() {
+    document.getElementById("crypto-form").classList.add("hidden");
+    document.getElementById("bank-form").classList.remove("hidden");
+    document.getElementById("credit-card-form").classList.add("hidden");
+    document.getElementById("paypal-form").classList.add("hidden");
+}
+
+function showCreditCardForm() {
+    document.getElementById("crypto-form").classList.add("hidden");
+    document.getElementById("bank-form").classList.add("hidden");
+    document.getElementById("credit-card-form").classList.remove("hidden");
+    document.getElementById("paypal-form").classList.add("hidden");
+}
+
+function showPayPalForm() {
+    document.getElementById("crypto-form").classList.add("hidden");
+    document.getElementById("bank-form").classList.add("hidden");
+    document.getElementById("credit-card-form").classList.add("hidden");
+    document.getElementById("paypal-form").classList.remove("hidden");
+}
